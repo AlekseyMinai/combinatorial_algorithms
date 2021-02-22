5b0109e83a22f543ad5f4573b7b4bfc429a04de7
AlekseyMinai <aleksejzuka@gmail.com>
2021-02-22 16:31:44 +0300

    изменил имя проекта

--- lab01/lab01/task01.cpp
+++ lab01/lab01/task01.cpp
@@ -5,16 +5,20 @@
 
 int main()
 {
-    std::vector<size_t> v = { 7, 1, 12 };
+    std::vector<size_t> v = { 7, 1, 12, 20, 30, 22, 10, 55, 1};
     boost::timer t;
     t.restart();
     std::sort(v.begin(), v.end());
     do
     {
-        copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
-        std::cout << std::endl;
+        //copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+        //std::cout << std::endl;
     } while (std::next_permutation(v.begin(), v.end()));
     double duration = t.elapsed();
     std::cout << duration << std::endl;
     return 0;
 }
+
+// for 10 - 0.045s
+// for 13 - 52.5s
+// for 20 ~ 235d
