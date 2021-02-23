d55691ccbfc4094d7419aab3ba4d6a495a8ed229
Aleksey Minai <aleksejzuka@gmail.com>
2021-02-24 00:46:44 +0300

    [lab01] t02

--- lab01/task02/task02.cpp
+++ lab01/task02/task02.cpp
@@ -1,132 +1,81 @@
 #include <string>
 #include <iostream>
 #include <vector>
-
-
-enum Direction
-{
-	LEFT, RIGHT
-};
-
-struct Element
-{
-	int value;
-	Direction direction;
-
-	Element(int value, Direction direction)
-	{
-		this->value = value;
-		this->direction = direction;
-	}
-
-	Element() {}
-
-};
-
-
-template<typename T, typename R>
-struct Pair
-{
-	T key;
-	R value;
-
-	Pair(T key, R value)
-	{
-		this->key = key;
-		this->value = value;
-	}
-};
-
-Pair<int, Element> get_largest_mobile_from(std::vector<Element> &elements, int last_largest_mobile_value)
-{
-	Element largest_mobile_element = Element(-1, LEFT);
-	int index = 0;
-	for (int i = 0; i < elements.size(); ++i)
-	{
-		bool is_larger_then_current_largest = elements[i].value > largest_mobile_element.value;
-		if (elements[i].direction == LEFT)
-		{
-			bool is_larger_then_previous = i != 0 && elements[i].value > elements[i - 1].value;
-			if ((i == 0 && elements[i].value == last_largest_mobile_value))// || 
-				//(elements[i].value < elements[i - 1].value && elements[i].value == last_largest_mobile_value))
-			{
-				elements[i].direction = RIGHT;
-				continue;
-			}
-			if (is_larger_then_previous && is_larger_then_current_largest)
-			{
-				largest_mobile_element = elements[i];
-				index = i;
-			}
-		}
-		else
-		{
-			if ((i == elements.size() - 1 && elements[i].value == last_largest_mobile_value)) //||
-				//(elements[i].value < elements[i + 1].value && elements[i].value == last_largest_mobile_value))
-			{
-				elements[i].direction = LEFT;
-				continue;
-			}
-			bool is_larger_then_next = i != elements.size() - 1 && elements[i].value > elements[i + 1].value;
-			if (is_larger_then_next && is_larger_then_current_largest)
-			{
-				largest_mobile_element = elements[i];
-				index = i;
-			}
-		}
-	}
-	//std::cout << largest_mobile_element.value << std::endl;
-	return Pair<int, Element>(index, largest_mobile_element);
-}
+#include <boost/timer.hpp>
 
 void generate_all_permutations(
-	std::vector<Element> elements,
-	void(*return_permutation)(std::vector<Element>))
+	std::vector<int> current_permutation, 
+	void(*return_permutation)(std::vector<int>&)
+)
 {
-	return_permutation(elements);
-	Pair<int, Element> result = get_largest_mobile_from(elements, 0);
-	int i = 0;
-	while (result.value.value != 1 && i < 25)
+	int max_element = current_permutation[current_permutation.size() -  1];
+	int border = max_element + 1;
+	current_permutation.insert(current_permutation.begin(), border);
+	current_permutation.push_back(border);
+
+	std::vector<int> reverse_permutation;
+	std::vector<int> directions;
+
+	for (int i = 0; i < current_permutation.size(); i++)
 	{
-		i++;
-		switch (result.value.direction)
-		{
-			case LEFT:
-			{
-				std::swap(elements[result.key], elements[result.key - 1]);
-				break;
-			}
-			case RIGHT:
-			{
-				std::swap(elements[result.key], elements[result.key + 1]);
-				break;
-			}
-		}
-		result = get_largest_mobile_from(elements, result.value.value);
-		return_permutation(elements);
+		reverse_permutation.push_back(current_permutation[i]);
+		directions.push_back(-1);
 	}
+	directions[1] = 0;
+
+	int moving_position = max_element;
+
+	boost::timer t;
+	t.restart();
+
+	while (moving_position != 1)
+	{
+		return_permutation(current_permutation);
+		moving_position = max_element;
+		while (current_permutation[reverse_permutation[moving_position] + directions[moving_position]] > moving_position)
+		{
+			directions[moving_position] = -directions[moving_position];
+			moving_position--;
+		}
+		std::swap(
+			current_permutation[reverse_permutation[moving_position]],
+			current_permutation[reverse_permutation[moving_position] + directions[moving_position]]
+		);
+		std::swap(
+			reverse_permutation[current_permutation[reverse_permutation[moving_position]]],
+			reverse_permutation[moving_position]
+		);
+	}
+
+	//loging computation time
+	double duration = t.elapsed();
+	std::cout << duration << std::endl;
 }
 
-int count = 1;
-
-void print(std::vector<Element> result)
+void print(std::vector<int> &v)
 {
-	std::cout << count << ". ";
-	count++;
-	for (int i = 0; i < result.size(); ++i)
+	//for (int i = 1; i < v.size() - 1; i++)
+	//{
+	//	std::cout << v[i] << " ";
+	//}
+	//std::cout << std::endl;
+}
+
+void generate_all_permutations(int size, void(*return_permutation)(std::vector<int>&))
+{
+	std::vector<int> first_permutation;
+	for (int i = 1; i < size + 1; ++i)
 	{
-		std::cout << result[i].value << " " << "(" << result[i].direction << "); ";
+		first_permutation.push_back(i);
 	}
-	std::cout << std::endl;
+	generate_all_permutations(first_permutation, return_permutation);
 }
 
 int main() {
-	std::vector<Element> elements;
-	for (int i = 0; i < 5; ++i)
-	{
-		elements.push_back(Element(i + 1, LEFT));
-	}
-	void(*printResult)(std::vector<Element>) = print;
-	generate_all_permutations(elements, printResult);
+	void(*print_result)(std::vector<int>&) = print;
+	generate_all_permutations(10, print_result); // = 0.48
+	generate_all_permutations(13, print_result); // = 46.04
+	//generate_all_permutations(15, print_result); ~ 46 * 14 * 15
+	//generate_all_permutations(20, print_result);
 	return 0;
 }
